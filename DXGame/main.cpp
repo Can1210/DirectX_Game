eaf6dc1f3a3275627e789cf93217f7df65f36dce
佐藤幹 <->
2019-12-04 20:17:58 +0900

    4

--- DXGame/main.cpp
+++ DXGame/main.cpp
@@ -1,12 +1,27 @@
-// include the basic windows header file
+// include the basic windows header files and the Direct3D header files
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+// global declarations
+IDXGISwapChain *swapchain;             // the pointer to the swap chain interface
+ID3D11Device *dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext *devcon;           // the pointer to our Direct3D device context
+
+// function prototypes
+void InitD3D(HWND hWnd);    // sets up and initializes Direct3D
+void CleanD3D(void);        // closes Direct3D and releases memory
 
 // the WindowProc function prototype
-LRESULT CALLBACK WindowProc(HWND hWnd,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 
 // the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -14,81 +29,125 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	// the handle for the window, filled by a function
 	HWND hWnd;
-	// this struct holds information for the window class
 	WNDCLASSEX wc;
 
-	// clear out the window class for use
 	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 
-	// fill in the struct with the needed information
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.lpszClassName = "WindowClass1";
+	wc.lpszClassName = "WindowClass";
 
-	// register the window class
 	RegisterClassEx(&wc);
 
-	// calculate the size of the client area
-	RECT wr = { 0, 0, 500, 400 };    // set the size, but not the position
-	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);    // adjust the size
+	RECT wr = { 0, 0, 800, 600 };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 
-	// create the window and use the result as the handle
 	hWnd = CreateWindowEx(NULL,
-		"WindowClass1",    // name of the window class
-		"Our First Windowed Program",   // title of the window
-		WS_OVERLAPPEDWINDOW,    // window style
-		300,    // x-position of the window
-		300,    // y-position of the window
-		wr.right - wr.left,    // width of the window
-		wr.bottom - wr.top,    // height of the window
-		NULL,    // we have no parent window, NULL
-		NULL,    // we aren't using menus, NULL
-		hInstance,    // application handle
-		NULL);    // used with multiple windows, NULL
+		"WindowClass",
+		"Our First Direct3D Program",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
 
-// display the window on the screen
 	ShowWindow(hWnd, nCmdShow);
+
+	// set up and initialize Direct3D
+	InitD3D(hWnd);
 
 	// enter the main loop:
 
-	// this struct holds Windows event messages
 	MSG msg;
 
-	// wait for the next message in the queue, store the result in 'msg'
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (TRUE)
 	{
-		// translate keystroke messages into the right format
-		TranslateMessage(&msg);
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
 
-		// send the message to the WindowProc function
-		DispatchMessage(&msg);
+			if (msg.message == WM_QUIT)
+				break;
+		}
+		else
+		{
+			// Run game code here
+			// ...
+			// ...
+		}
 	}
 
-	// return this part of the WM_QUIT message to Windows
+	// clean up DirectX and COM
+	CleanD3D();
+
 	return msg.wParam;
 }
+
 
 // this is the main message handler for the program
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	// sort through and find what code to run for the message given
 	switch (message)
 	{
-		// this message is read when the window is closed
 	case WM_DESTROY:
 	{
-		// close the application entirely
 		PostQuitMessage(0);
 		return 0;
 	} break;
 	}
 
-	// Handle any messages the switch statement didn't
 	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd)
+{
+	// create a struct to hold information about the swap chain
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	// clear out the struct for use
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	scd.BufferCount = 1;                                    // one back buffer
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	scd.OutputWindow = hWnd;                                // the window to be used
+	scd.SampleDesc.Count = 4;                               // how many multisamples
+	scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+	// create a device, device context and swap chain using the information in the scd struct
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+}
+
+
+// this is the function that cleans up Direct3D and COM
+void CleanD3D(void)
+{
+	// close and release all existing COM objects
+	swapchain->Release();
+	dev->Release();
+	devcon->Release();
 }
